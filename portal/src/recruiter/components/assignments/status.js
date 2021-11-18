aa4c4514c41843fd0c6f1758de33739f257120de
Hugo <hugorut@gmail.com>
2021-11-18 10:38:11 +0000

    fix(portal): ui issues with tailwind classes not being included in prod build

--- portal/src/recruiter/components/assignments/status.js
+++ portal/src/recruiter/components/assignments/status.js
@@ -1,38 +1,38 @@
 import React from "react";
 
 const AssignmentStatus = (props) => {
-    let colour = "orange";
+    let colour = "bg-orange-200";
 
     switch (props.status) {
         case "unsent":
-            colour = "gray";
+            colour = "bg-gray-200";
             break;
         case "sending":
-            colour = "gray";
+            colour = "bg-gray-200";
             break;
         case "sent":
-            colour = "blue";
+            colour = "bg-blue-200";
             break;
         case "scheduled":
-            colour = "pink"
+            colour = "bg-pink-200"
             break;
         case "in progress":
-            colour = "yellow";
+            colour = "bg-yellow-200";
             break;
         case "submitted":
-            colour = "green";
+            colour = "bg-green-200";
             break;
         case "missed":
-            colour = "red";
+            colour = "bg-red-200";
             break;
         default:
-            colour = "orange"
+            colour = "bg-orange-200"
     }
 
     return (
         <span
             className={"relative inline-block px-4 py-1 font-semibold text-gray-700 text-xs leading-tight"}>
-            <span aria-hidden className={"absolute inset-0 bg-" + colour + "-200 opacity-50 rounded-full"}/>
+            <span aria-hidden className={"absolute inset-0 " + colour + " opacity-50 rounded-full"}/>
             <span className="relative">{props.status}</span>
         </span>
     )
